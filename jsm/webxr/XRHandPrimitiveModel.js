0a432adc35b43a09f51cfd7614bef74cbc0a04d5
Birat Poudel <poudel.birat25@gmail.com>
2022-02-08 11:53:35 +0545

    paths updated

--- jsm/webxr/XRHandPrimitiveModel.js
+++ jsm/webxr/XRHandPrimitiveModel.js
@@ -4,36 +4,36 @@ import {
 	MeshStandardMaterial,
 	Mesh,
 	Group
-} from '../../../build/three.module.js';
+} from '../../build/three.module.js';
 
 class XRHandPrimitiveModel {
 
-	constructor( handModel, controller, path, handedness, options ) {
+	constructor(handModel, controller, path, handedness, options) {
 
 		this.controller = controller;
 		this.handModel = handModel;
 
-	  this.envMap = null;
+		this.envMap = null;
 
 		this.handMesh = new Group();
-		this.handModel.add( this.handMesh );
+		this.handModel.add(this.handMesh);
 
-		if ( window.XRHand ) {
+		if (window.XRHand) {
 
 			let geometry;
 
-			if ( ! options || ! options.primitive || options.primitive === 'sphere' ) {
+			if (!options || !options.primitive || options.primitive === 'sphere') {
 
-				geometry = new SphereBufferGeometry( 1, 10, 10 );
+				geometry = new SphereBufferGeometry(1, 10, 10);
 
-			} else if ( options.primitive === 'box' ) {
+			} else if (options.primitive === 'box') {
 
-				geometry = new BoxBufferGeometry( 1, 1, 1 );
+				geometry = new BoxBufferGeometry(1, 1, 1);
 
 			}
 
-			const jointMaterial = new MeshStandardMaterial( { color: 0xffffff, roughness: 1, metalness: 0 } );
-			const tipMaterial = new MeshStandardMaterial( { color: 0x999999, roughness: 1, metalness: 0 } );
+			const jointMaterial = new MeshStandardMaterial({ color: 0xffffff, roughness: 1, metalness: 0 });
+			const tipMaterial = new MeshStandardMaterial({ color: 0x999999, roughness: 1, metalness: 0 });
 
 			const tipIndexes = [
 				window.XRHand.THUMB_PHALANX_TIP,
@@ -42,12 +42,12 @@ class XRHandPrimitiveModel {
 				window.XRHand.RING_PHALANX_TIP,
 				window.XRHand.LITTLE_PHALANX_TIP
 			];
-			for ( let i = 0; i <= window.XRHand.LITTLE_PHALANX_TIP; i ++ ) {
+			for (let i = 0; i <= window.XRHand.LITTLE_PHALANX_TIP; i++) {
 
-				var cube = new Mesh( geometry, tipIndexes.indexOf( i ) !== - 1 ? tipMaterial : jointMaterial );
+				var cube = new Mesh(geometry, tipIndexes.indexOf(i) !== - 1 ? tipMaterial : jointMaterial);
 				cube.castShadow = true;
 				cube.receiveShadow = true;
-				this.handMesh.add( cube );
+				this.handMesh.add(cube);
 
 			}
 
@@ -63,16 +63,16 @@ class XRHandPrimitiveModel {
 		// XR Joints
 		const XRJoints = this.controller.joints;
 
-		for ( let i = 0; i < objects.length; i ++ ) {
+		for (let i = 0; i < objects.length; i++) {
 
-			const jointMesh = objects[ i ];
-			const XRJoint = XRJoints[ i ];
+			const jointMesh = objects[i];
+			const XRJoint = XRJoints[i];
 
-			if ( XRJoint.visible ) {
+			if (XRJoint.visible) {
 
-				jointMesh.position.copy( XRJoint.position );
-				jointMesh.quaternion.copy( XRJoint.quaternion );
-				jointMesh.scale.setScalar( XRJoint.jointRadius || defaultRadius );
+				jointMesh.position.copy(XRJoint.position);
+				jointMesh.quaternion.copy(XRJoint.quaternion);
+				jointMesh.scale.setScalar(XRJoint.jointRadius || defaultRadius);
 
 			}
 
